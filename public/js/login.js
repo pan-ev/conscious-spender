0794db2d32ab5b486bef972a0bd714e247ebc9c1
Elinor Chayo <elliechayo@gmail.com>
2023-05-01 20:18:54 -0400

    declared query selectors for buttons, sections, forms and divs. created the handleloginsubmit/user authentication function

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,5 @@
 function main()  {
   //buttons
-
   const signInButtons = document.querySelectorAll(".signin-button");
   const registerButtons = document.querySelectorAll(".register-button");
   const forgotButtons = document.querySelectorAll(".forgot-button");
@@ -18,13 +17,33 @@ function main()  {
 
   // error divs
   const loginError = document.querySelector(".loginError");
-  const registerError = document.querySelector(".registerError")
-
+  const registerError = document.querySelector(".registerError");
 
 async function handleLoginSubmit(event) {
   event.preventDefault();
   const reqBody = {
-    email: event.target.querySelector("input")
+    email: event.target.querySelector("input[email]").value,
+    password: event.target.querySelector("input[password]").value,
+  };
+  try {
+    const res = await fetch("/api/user/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      }
+      body: JSON.stringify(reqBody),
+    });
+    const data = await res.json();
+    if (data.success === true) {
+      console.log(data.user);
+      alert("Login successful!");
+      signInForm.reset();
+    }else {
+      loginError.innerText = data.message || "something went wrong!";
+    }
+  } catch (error) {
+    console.error(error);
+    loginError.innerText = "Something went wrong!";
   }
 }
 
