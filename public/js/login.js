9670cb7a1db22f2f9ed0b749c1f9d2b3bc785ed0
Elinor Chayo <elliechayo@gmail.com>
2023-05-01 20:41:49 -0400

    modified public/login.js: added resgiter(form) function and added event listeners for the register form and the signin form. corrected the syntax login function bofy object values.

--- public/js/login.js
+++ public/js/login.js
@@ -13,24 +13,26 @@ function main()  {
 
   //forms
   const signInForm = document.querySelector("#signInForm");
-  const registerForm = document.querySelector("#registerfrom");
+  const registerForm = document.querySelector("#registerForm");
 
   // error divs
   const loginError = document.querySelector(".loginError");
   const registerError = document.querySelector(".registerError");
 
+
+  // login function
 async function handleLoginSubmit(event) {
   event.preventDefault();
   const reqBody = {
-    email: event.target.querySelector("input[email]").value,
-    password: event.target.querySelector("input[password]").value,
+    email: event.target.querySelector("input[name='email]").value,
+    password: event.target.querySelector("input[name='password]").value,
   };
   try {
     const res = await fetch("/api/user/login", {
       method: "POST",
       headers: {
         "content-type": "application/json",
-      }
+      },
       body: JSON.stringify(reqBody),
     });
     const data = await res.json();
@@ -46,6 +48,42 @@ async function handleLoginSubmit(event) {
     loginError.innerText = "Something went wrong!";
   }
 }
+//register function
+async function handleRegisterSubmit(event) {
+  event.preventDefault();
+  const reqBody = {
+    email: event.target.querySelector("input[name='email'").value,
+    password: event.target.querySelector("input[name='password'").value,
+    firstName: event.target.querySelector("input[name='firstName']").value,
+    lastName: event.target.querySelector("input[name='lastname']").value,
+    phone: event.target.querySelector("input[name='phone']").value,
+  };
+  try {
+    const res = await fetch("/api/users/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(reqBody),
+    });
+    const data = await res.json();
+    if (data.success === true) {
+      console.log(data.user);
+      alert("registration successful!");
+      registrationForm.reset();
+    } else {
+      registerError.innerText = data.message || " Something went wrong!";
+    }
+  } catch (error) {
+    console.log(error);
+    registerError.innerText = "Something went wrong";
+  }
+}
+
+registerForm.addEventListener("submit", handleRegisterSubmit);
+signInForm.addEventListener("submit", handleLoginSubmit);
+
+
 
 
 
