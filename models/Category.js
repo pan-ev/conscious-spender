58fd64c7937d6959d01e76061e8c100c2ae11539
epan <epan616@gmail.com>
2023-05-04 19:24:59 -0400

    Model Updates and Login CSS

--- models/Category.js
+++ models/Category.js
@@ -1,29 +1,29 @@
-const { Model, DataTypes } = require("sequelize");
+// const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require("../config/connection");
+// const sequelize = require("../config/connection");
 
-class Category extends Model {}
+// class Category extends Model {}
 
-Category.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "category",
-  }
-);
+// Category.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     category_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "category",
+//   }
+// );
 
-module.exports = Category;
+// module.exports = Category;
