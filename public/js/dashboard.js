edc70cd61a5ca10a83bdc2291b07b5148dc9a066
Elinor Chayo <elliechayo@gmail.com>
2023-05-06 14:05:46 -0400

    fixed fetch transcations function

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,96 +1,98 @@
 const totalExpenseCanvas = document.querySelector("#totalExpenseChart");
 const categoryExpenseCanvas = document.querySelector("#categoryExpenseChart");
 
-
-
 // creates and renders total expense chart
 function createTotalExpenseChart(data) {
-
+     // we need to parse only 
+     // the required fields from the data
      const parsedData = {};
      data.forEach(d => {
-          const date = new Date(d.transaction_date).toLocaleDateString();
-          if(parseData[date] === undefined) {
-               parseData[date] = number(d.transaction_amount);
-          } else {
-               parseData[date] += number(d.transaction_amount);
-          }
+         // convert the transaction_date to a Date object
+         const date = new Date(d.transaction_date).toLocaleDateString();
+         // check if parsedData already has the date
+         // if true, add the amount to the already existing date
+         // if false, set the date in parsedDate,
+         if (parsedData[date] === undefined) {
+             parsedData[date] = Number(d.transaction_amount);
+         } else {
+             parsedData[date] += Number(d.transaction_amount);
+         }
      });
-
-
-    // create the chart using the Chart.js module
-    new Chart(totalExpenseCanvas, {
-     type: "bar",
-     date: {
-          lable: Object.keys(parsedData),
-          database: [
-               {
-                    lable: "Amount",
-                    data: Object.values(parsedData),
-                    borderWidth: 1,
-               }
-          ]
-     },
-     options: {
-          scales: {
-               y: {
-                    beginAtZero: true,
-               }
-          }
-     }
-  });
-
-}
-
-// creates and renders category expense chart
-function createCategoryExpenseChart(data) {
-
-     const parseData = {};
+ 
+     // create the chart using the Chart.js module
+     new Chart(totalExpenseCanvas, {
+         type: "bar",
+         data: {
+             labels: Object.keys(parsedData),
+             datasets: [
+                 {
+                     label: "Amount",
+                     data: Object.values(parsedData),
+                     borderWidth: 1,
+                 }
+             ]
+         },
+         options: {
+             scales: {
+                 y: {
+                     beginAtZero: true,
+                 }
+             }
+         }
+     });
+ }
+ 
+ // creates and renders category expense chart
+ function createCategoryExpenseChart(data) {
+     // here, we need to create key-value pair of category-amount
+     const parsedData = {};
+     // loop through the data and parse the required field
      data.forEach(d => {
-          const category = d.category.toLowerCase()
-               if (parseData[category] === undefined) {
-                    parseData[category] = Number(d.transaction_amount);
-               } else {
-                    parseData[category] += Number(d.transaction_amount);
-               }
-           });
-
-           // create the chart using the parsed data
-           new Chart(categoryExpenseCanvas, {
-               type: "pie",
-               data: {
-                    lables: Object.keys(parseData),
-                    database: [
-                         { 
-                              lable: "Amount", 
-                              data: Object.values(parseData)
-                         }
-                    ]
-               },
-               options: { 
-                    maintainAspectRatio: false,
-                    reponsive: true,
-                    plugin: {
-                         legend: {
-                              position: "right"
-                         },
-                    },
-               },
-           })
-
-          }
-// Fetch the transactions from our API
-async function fetchTransaction() {
+         const category = d.category.toLowerCase()
+         // if no already existing data in parsedData, create a new one
+         if (parsedData[category] === undefined) {
+             parsedData[category] = Number(d.transaction_amount);
+         } else {
+             // add the amount to the already existing key in parsedData
+             parsedData[category] += Number(d.transaction_amount);
+         }
+     });
+ 
+     // create the chart using the parsed data
+     new Chart(categoryExpenseCanvas, {
+         type: "pie",
+         data: {
+             labels: Object.keys(parsedData),
+             datasets: [{ label: "Amount", data: Object.values(parsedData) }],
+         },
+         options: {
+             maintainAspectRatio: false,
+             responsive: true,
+             plugins: {
+                 legend: {
+                     position: "right",
+                 },
+             },
+         },
+     })
+ 
+ }
+ 
+ 
+ // Fetch the transactions from our API
+ async function fetchTransactions() {
+     // using trycatch block to catch any errors
      try {
-          const response = await fetch("/api/transaction");
-          const data = await response.json();
-
-          createTotalExpenseChart(data);
-          createCategoryExpenseChart(data);
+         const response = await fetch("/api/transactions");
+         const data = await response.json();
+ 
+         // create charts using the data returned from the API
+         createTotalExpenseChart(data);
+         createCategoryExpenseChart(data);
      } catch (error) {
-          console.log(error.message);
+         // log the error message to the console
+         console.log(error.message);
      }
-}
-
-fetchTransaction()
-
-
+ }
+ 
+ fetchTransactions()
